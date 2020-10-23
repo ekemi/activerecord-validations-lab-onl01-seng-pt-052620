0a85460b7559fa8edc1f7c32af653f816e117d6e
edmundo papi <edmundopierre@yahoo.com>
2020-10-23 01:50:54 +0000

    Automatically backed up by Learn

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,5 +1,5 @@
 class Post < ActiveRecord::Base
   validates :title, presence: true
-  #validates :content, length: {minimun: 250}
+  validates :category, inclusion: {in: %w(Fiction Non-Fiction)}
   validates :summary, length: {maximum: 250}
 end
