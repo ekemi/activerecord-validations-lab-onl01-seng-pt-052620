3f3efa9b7a58ac9643b57fa4e4194786d47530f2
edmundo papi <edmundopierre@yahoo.com>
2020-10-23 02:37:15 +0000

    Automatically backed up by Learn

--- app/helpers/my_validator.rb
+++ app/helpers/my_validator.rb
@@ -1,14 +1,11 @@
-class Myvalidator <ActiveModel::validator
 
-def validate(record)
-
-if record.title
-  reqs = ["Won't Believe", "Secret", "Top[number]", "Guess"]
-  if reqs.detect{|el| record.title.include?(el)}.nil?
-    record.errors[:title] <<  "Must contain clickbait"
-
-end
-
-end
-end
-end
+class MyValidator < ActiveModel::Validator
+  def validate(record)
+    if record.title
+      reqs = ["Won't Believe", "Secret", "Top[number]", "Guess"]
+      if reqs.detect {|el| record.title.include?(el) }.nil?
+        record.errors[:title] << "Must contain clickbait"
+      end
+    end
+  end
+end 
